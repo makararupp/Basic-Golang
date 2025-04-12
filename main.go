3e2106e66d63c507e670e44881d457d4a905e4f9
makaraDevIt <makarasam68@gmail.com>
2025-04-12 12:48:30 +0700

    variable

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 import "fmt"
 
+/* Declare a single variable */
+var a int
+
+/* Declare a group of variables */
+var(
+	b bool
+	c float32
+	d string 
+)
+
 func main()  {
-	fmt.Println("Welcome to new developer")
-	
+	 a =42  // Assign single value
+	 b,c = true, 32.0  // Assign multiple values
+	 d ="makara"   // Strings must contain double quotes
+    fmt.Println(a, b, c, d)  //42, true, 32.0, makara
 }	
