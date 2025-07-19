b825a5c38ff3f0275a73f2b8bbc3dd05bd30fae0
makaraDevIt <makarasam68@gmail.com>
2025-07-19 12:09:22 +0700

    variable_refactored

---
+++ variable_refactoed.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	 a:= 42     // initializatiion and assign to a single variable
+	 b, c := true,  32.0 // initialization and to multiple variables
+	 d := "Hello"
+	 fmt.Println(a, b, c, d) 
+	 //output : 42, true, 32 Hello
+}
